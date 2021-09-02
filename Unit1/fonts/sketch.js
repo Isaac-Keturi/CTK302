1cb12641a6d6c1f7dffb26e55e1cd42287231aa5
Isaac-Keturi <77292598+Isaac-Keturi@users.noreply.github.com>
2021-09-02 09:58:13 -0500

    Fonts

--- Unit1/fonts/sketch.js
+++ Unit1/fonts/sketch.js
@@ -1,13 +1,16 @@
-var font1 ;
+var font1 ; font2
 
 
 function setup() {
   createCanvas(500, 500);
   font1 = loadFont("assets/HotSweat.ttf") ;
+  font2= loadFont("assets/SnowKei.ttf") ;
 }
 
 function draw() {
   background("purple") ;
   textFont(font1, 48) ;
   text("Hello World", 100, 100) ;
+  textFont(font2, 58) ;
+  text("Hello World", 20, 200) ;
 }
