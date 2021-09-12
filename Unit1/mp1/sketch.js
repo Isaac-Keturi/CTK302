7ae3714aa569281fc74c6c1ad0975ab76b14c13c
Isaac-Keturi <77292598+Isaac-Keturi@users.noreply.github.com>
2021-09-12 12:26:18 -0500

    mp1

--- Unit1/mp1/sketch.js
+++ Unit1/mp1/sketch.js
@@ -11,37 +11,46 @@ function draw() {
 
   background(100);
 
-
   if (mouseIsPressed) {
+
+    background("red");
+    fill('white');
+    triangle(497, 257, 429, 371, 579, 371);
+
+    ellipse(width / 2, 200, 200, 200);
+
+    quad(463, 318, 449, 340, 409, 314, 421, 298);
+    quad(538, 313, 551, 334, 578, 312, 572, 303);
+    rect(457, 393, 25, 50);
+    rect(537, 393, 25, 50);
+    circle(462, 162, 40);
+    circle(552, 162, 40);
+    arc(500, 272, 90, 100, 280, 0, PIE);
+    triangle(474, 230, 485, 230, 477, 265);
+    triangle(514, 230, 524, 230, 517, 265);
+    quad(469, 123, 491, 144, 480, 157, 445, 133);
+    quad(542, 122, 565, 135, 534, 152, 525, 149);
+    textSize(10);
+    text('HOLY S*** EVERYBODY GET OUT OF HERE ITS SPOOKY GUY', 10, 30);
+
     // stuff that shows when the mouse is pressed
 
   } else {
+    fill('white');
+    triangle(497, 257, 429, 371, 579, 371)
 
-    ellipse( width/2, 200, 200, 200)
-    fill(white) ;
+    ellipse(width / 2, 200, 200, 200)
 
-
-    triangle(30, 75, 58, 20, 86, 75)
-    fill(white) ;
-    // when the mouse isn't pressed!
-
+    quad(463, 318, 449, 340, 409, 314, 421, 298);
+    quad(538, 313, 551, 334, 578, 312, 572, 303);
+    rect(457, 393, 25, 50);
+    rect(537, 393, 25, 50);
+    circle(462, 162, 40);
+    circle(552, 162, 40);
+    arc(500, 242, 80, 80, 0, PI + QUARTER_PI, PIE);
+    textSize(10);
+    text('This is dude the vampire guy. Look how spooky he is. Oh geez what a spooky dude', 10, 30);
   }
 
 
-
-  // this shows mouse location - comment it out when you're done!
-
-  fill(100);
-  text(mouseX + ", " + mouseY, 40, 40);
-
-}
-
-
-
-
-
-
-// record the mouse location in console when clicked
-function mouseReleased() {
-  print(mouseX + ", " + mouseY);
 }
